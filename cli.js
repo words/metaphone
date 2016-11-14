076d7555308343a9dc4028b213f31739f226a70c
Titus Wormer <tituswormer@gmail.com>
2016-11-14 14:04:55 +0100

    Refactor code-style

--- cli.js
+++ cli.js
@@ -1,109 +1,82 @@
 #!/usr/bin/env node
 'use strict';
 
-/* eslint-disable no-console */
-
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var pack = require('./package.json');
 var metaphone = require('./');
 
-/*
- * Detect if a value is expected to be piped in.
- */
-
+/* Detect if a value is expected to be piped in. */
 var expextPipeIn = !process.stdin.isTTY;
 
-/*
- * Arguments.
- */
-
+/* Arguments. */
 var argv = process.argv.slice(2);
 
-/*
- * Command.
- */
-
+/* Command. */
 var command = pack.name;
-
-/**
- * Get the distance for a word.
- *
- * @param {Array.<string>} values - List of words.
- * @return {string}
- */
-function phonetics(values) {
-    return values.map(metaphone).join(' ');
-}
-
-/**
- * Help.
- *
- * @return {string}
- */
-function help() {
-    return [
-        '',
-        'Usage: ' + command + ' [options] <words...>',
-        '',
-        pack.description,
-        '',
-        'Options:',
-        '',
-        '  -h, --help           output usage information',
-        '  -v, --version        output version number',
-        '',
-        'Usage:',
-        '',
-        '# output phonetics',
-        '$ ' + command + ' considerations detestable',
-        '# ' + phonetics(['considerations', 'detestable']),
-        '',
-        '# output phonetics from stdin',
-        '$ echo "hiccups vileness" | ' + command,
-        '# ' + phonetics(['hiccups', 'vileness']),
-        ''
-    ].join('\n  ') + '\n';
-}
-
-/**
- * Get the phonetics for zero or more words.
- *
- * @param {Array.<string>} values - List of words.
- */
-function getPhonetics(values) {
-    if (values.length) {
-        console.log(phonetics(values));
-    } else {
-        process.stderr.write(help());
-        process.exit(1);
-    }
-}
 
 /*
  * Program.
  */
 
 if (
-    argv.indexOf('--help') !== -1 ||
-    argv.indexOf('-h') !== -1
+  argv.indexOf('--help') !== -1 ||
+  argv.indexOf('-h') !== -1
 ) {
-    console.log(help());
+  console.log(help());
 } else if (
-    argv.indexOf('--version') !== -1 ||
-    argv.indexOf('-v') !== -1
+  argv.indexOf('--version') !== -1 ||
+  argv.indexOf('-v') !== -1
 ) {
-    console.log(pack.version);
-} else if (argv.length) {
-    getPhonetics(argv.join(' ').split(/\s+/g));
-} else if (!expextPipeIn) {
-    getPhonetics([]);
+  console.log(pack.version);
+} else if (argv.length !== 0) {
+  getPhonetics(argv.join(' ').split(/\s+/g));
+} else if (expextPipeIn) {
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', function (data) {
+    getPhonetics(data.trim().split(/\s+/g));
+  });
 } else {
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('data', function (data) {
-        getPhonetics(data.trim().split(/\s+/g));
-    });
+  getPhonetics([]);
+}
+
+/* Help. */
+function help() {
+  return [
+    '',
+    'Usage: ' + command + ' [options] <words...>',
+    '',
+    pack.description,
+    '',
+    'Options:',
+    '',
+    '  -h, --help           output usage information',
+    '  -v, --version        output version number',
+    '',
+    'Usage:',
+    '',
+    '# output phonetics',
+    '$ ' + command + ' considerations detestable',
+    '# ' + phonetics(['considerations', 'detestable']),
+    '',
+    '# output phonetics from stdin',
+    '$ echo "hiccups vileness" | ' + command,
+    '# ' + phonetics(['hiccups', 'vileness']),
+    ''
+  ].join('\n  ') + '\n';
+}
+
+/* Get the phonetics for zero or more words. */
+function getPhonetics(values) {
+  if (values.length === 0) {
+    process.stderr.write(help());
+    process.exit(1);
+  } else {
+    console.log(phonetics(values));
+  }
+}
+
+/* Get the distance for a word. */
+function phonetics(values) {
+  return values.map(metaphone).join(' ');
 }
--- test/extensive.js
+++ test/extensive.js
@@ -7,29 +7,21 @@ var assert = require('assert');
 var wordList = require('word-list');
 var metaphone = require('..');
 
-/*
- * Methods.
- */
-
 var readFile = fs.readFileSync;
 var equal = assert.equal;
-
-/*
- * Fixtures.
- */
 
 var words = readFile(wordList, 'utf-8').split('\n');
 var baseline = readFile('./test/fixtures.txt', 'utf-8').split('\n');
 
 describe('metaphone()', function () {
-    words.forEach(function (value, index) {
-        it('should work on "' + value + '"', function () {
-            try {
-                equal(metaphone(value), baseline[index]);
-            } catch (e) {
-                e.message += ' (source: ' + value + ')'
-                throw e;
-            }
-        });
+  words.forEach(function (value, index) {
+    it('should work on "' + value + '"', function () {
+      try {
+        equal(metaphone(value), baseline[index]);
+      } catch (err) {
+        err.message += ' (source: ' + value + ')';
+        throw err;
+      }
     });
+  });
 });
