a2206b27594733b16ad1f24df731a6d1db0836d5
Titus Wormer <tituswormer@gmail.com>
2016-11-14 15:23:33 +0100

    Refactor `cli.js`

--- cli.js
+++ cli.js
@@ -5,19 +5,10 @@
 var pack = require('./package.json');
 var metaphone = require('./');
 
-/* Detect if a value is expected to be piped in. */
-var expextPipeIn = !process.stdin.isTTY;
-
 /* Arguments. */
 var argv = process.argv.slice(2);
 
-/* Command. */
-var command = pack.name;
-
-/*
- * Program.
- */
-
+/* Program. */
 if (
   argv.indexOf('--help') !== -1 ||
   argv.indexOf('-h') !== -1
@@ -28,23 +19,26 @@ if (
   argv.indexOf('-v') !== -1
 ) {
   console.log(pack.version);
-} else if (argv.length !== 0) {
-  getPhonetics(argv.join(' ').split(/\s+/g));
-} else if (expextPipeIn) {
+} else if (argv.length === 0) {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', function (data) {
-    getPhonetics(data.trim().split(/\s+/g));
+    console.log(phonetics(data));
   });
 } else {
-  getPhonetics([]);
+  console.log(phonetics(argv.join(' ')));
+}
+
+/* Core. */
+function phonetics(values) {
+  return values.split(/\s+/g).map(metaphone).join(' ');
 }
 
 /* Help. */
 function help() {
   return [
     '',
-    'Usage: ' + command + ' [options] <words...>',
+    'Usage: ' + pack.name + ' [options] <words...>',
     '',
     pack.description,
     '',
@@ -56,27 +50,15 @@ function help() {
     'Usage:',
     '',
     '# output phonetics',
-    '$ ' + command + ' considerations detestable',
-    '# ' + phonetics(['considerations', 'detestable']),
+    '$ ' + pack.name + ' considerations detestable',
+    '# ' + phonetics('considerations detestable'),
     '',
     '# output phonetics from stdin',
-    '$ echo "hiccups vileness" | ' + command,
-    '# ' + phonetics(['hiccups', 'vileness']),
-    ''
+    '$ echo \'hiccups vileness\' | ' + pack.name,
+    '# ' + phonetics('hiccups vileness'),
+    '',
+    '# with stemmer',
+    '$ echo \'vileness\' | stemmer | ' + pack.name,
+    '# ' + phonetics('vile')
   ].join('\n  ') + '\n';
-}
-
-/* Get the phonetics for zero or more words. */
-function getPhonetics(values) {
-  if (values.length === 0) {
-    process.stderr.write(help());
-    process.exit(1);
-  } else {
-    console.log(phonetics(values));
-  }
-}
-
-/* Get the distance for a word. */
-function phonetics(values) {
-  return values.map(metaphone).join(' ');
 }
